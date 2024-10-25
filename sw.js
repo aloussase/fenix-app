a2d223dda11ba2e514456da7c45795ceadeedf0d
Alexander Goussas <goussasalexander@gmail.com>
2024-10-25 12:39:08 -0500

    update cached urls in service worker

--- sw.js
+++ sw.js
@@ -1,13 +1,14 @@
 const CACHE_NAME = "fenix-app-cache-v6";
 const CACHED_URLS = [
   // HTML
-  "./",
-  "./index.html",
+  "/",
+  "/index.html",
   // Images
-  "./assets/lightbulb.png",
-  "./assets/lightbulb-off.png",
+  "/assets/lightbulb.png",
+  "/assets/lightbulb-off.png",
   // Scripts
-  "./build/main.js",
+  "/main.js",
+  "/db.js",
   // StyleSheets
   "https://cdn.jsdelivr.net/npm/beercss@3.7.10/dist/cdn/beer.min.css",
   "https://cdn.jsdelivr.net/npm/beercss@3.7.10/dist/cdn/beer.min.js",
